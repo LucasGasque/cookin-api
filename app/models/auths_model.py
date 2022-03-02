45fe5a36ddc8b03744176bc21f26d6caa9a43069
Raissa Toledo <raissalst@gmail.com>
2022-03-02 10:25:05 -0300

    feat: implementing auths table

---
+++ app/models/auths_model.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from uuid import uuid4
+
+from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import validates
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app.configs.database import db
+
+
+@dataclass
+class AuthModel(db.Model):
+
+    __tablename__ = "auths"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid4)
+    email: str = Column(String(124), nullable=False, unique=True)
+    password_hash = Column(String(511), nullable=False)
+
+    @property
+    def password(self):
+        raise AttributeError("Not allowed to access password")
+
+    @password.setter
+    def password(self, password_to_hash):
+        self.password_hash = generate_password_hash(password_to_hash)
+
+    def check_password(self, password_to_compare):
+        return check_password_hash(self.password_hash, password_to_compare)
+
+    @validates("email")
+    def normalize_email(self, key, value):
+        return value.lower()
