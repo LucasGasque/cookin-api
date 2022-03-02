938b7fdae5ba3f4df0da54dc96a543782197ef52
Raissa Toledo <raissalst@gmail.com>
2022-03-02 12:25:58 -0300

    feat: changing order of blueprints

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -6,5 +6,5 @@ bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 bp_auth.post("")(auth.register_user)
 bp_auth.post("/login")(auth.login_user)
-bp_auth.delete("")(auth.delete_user)
 bp_auth.patch("")(auth.update_user)
+bp_auth.delete("")(auth.delete_user)
