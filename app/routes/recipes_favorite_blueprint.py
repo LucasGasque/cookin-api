58d8765cdb324801c8e762106443492fcb76042e
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-02 18:27:20 -0300

    refactor: fix typing from str to string

--- app/routes/recipes_favorite_blueprint.py
+++ app/routes/recipes_favorite_blueprint.py
@@ -4,8 +4,8 @@ from app.controllers import favorite_recipes
 
 bp_recipes_favorite = Blueprint("bp_recipes_favorite", __name__, url_prefix="/recipes")
 
-bp_recipes_favorite.post("/<str:recipe_id>/favorites")(favorite_recipes.favorite_recipe)
+bp_recipes_favorite.post("/<string:recipe_id>/favorites")(favorite_recipes.favorite_recipe)
 
-bp_recipes_favorite.get("/<str:recipe_id>/favorites")(favorite_recipes.get_favorite_recipe)
+bp_recipes_favorite.get("/<string:recipe_id>/favorites")(favorite_recipes.get_favorite_recipe)
 
-bp_recipes_favorite.delete("/<str:recipe_id>/favorites")(favorite_recipes.delete_favorite_recipe)
+bp_recipes_favorite.delete("/<string:recipe_id>/favorites")(favorite_recipes.delete_favorite_recipe)
