653298063f528ef175a908f417f1a27e84f1c5a5
Raissa Toledo <raissalst@gmail.com>
2022-03-02 10:59:23 -0300

    feat: implemented auth route

---
+++ app/routes/auth_route.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+from app.controllers import auth
+
+bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
+
+bp_auth.post("")(auth.register_user)
+bp_auth.post("/login")(auth.login_user)
+bp_auth.delete("")(auth.delete_user)
+bp_auth.patch("")(auth.update_user)
