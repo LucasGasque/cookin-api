49c7973c37f2cdd46c5ad21805549649d250001e
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-02 14:29:55 -0300

    feat: implemented recipes get route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask, Blueprint
 
+from app.routes.recipes_blueprint import bp_recipes
+
 bp_api = Blueprint("api", __name__, url_prefix="/api")
 
-
 def init_app(app: Flask):
-
     # bps here
 
     app.register_blueprint(bp_api)
+    
+    bp_api.register_blueprint(bp_recipes)
