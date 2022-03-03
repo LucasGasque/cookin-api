567996a9d53483dfab5ea70b6af1ec1755bec766
Raissa Toledo <raissalst@gmail.com>
2022-03-03 19:26:46 -0300

    feat: deleting recipes implemented

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask, Blueprint
 
 from app.routes.auth_route import bp_auth
 from app.routes.users_route import bp_users
-from app.routes.recipes_blueprint import bp_recipes
-from app.routes.recipes_favorite_blueprint import bp_recipes_favorite
+from app.routes.recipes_route import bp_recipes
+from app.routes.recipes_favorite_route import bp_recipes_favorite
 from app.routes.recipes_rating_route import bp_recipes_rating
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
@@ -13,8 +13,8 @@ def init_app(app: Flask):
     bp_api.register_blueprint(bp_auth)
     bp_api.register_blueprint(bp_users)
     bp_api.register_blueprint(bp_recipes)
-    
     bp_api.register_blueprint(bp_recipes_favorite)
     bp_api.register_blueprint(bp_recipes_rating)
+
 
     app.register_blueprint(bp_api)
