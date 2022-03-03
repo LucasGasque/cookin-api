051d6c9546f1b4edb5e7d1e2208de3e16e1ad6f5
Manoela Cunha <manoela_fg@hotmail.com>
2022-03-03 15:41:27 -0300

    feat: get_recipes_by_category controller creation

--- app/controllers/recipes/__init__.py
+++ app/controllers/recipes/__init__.py
@@ -1,1 +1,2 @@
 from .get_recipes import get_recipes
+from .get_recipes_by_category import get_recipes_by_category
--- app/routes/recipes_route.py
+++ app/routes/recipes_route.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
 
-from app.controllers.recipes.get_recipes import get_recipes
+from app.controllers.recipes import get_recipes, get_recipes_by_category
 
 bp_recipes = Blueprint("bp_recipes", __name__, url_prefix="/recipes")
 
 bp_recipes.get("")(get_recipes)
+
+bp_recipes.get("/<string:category>")(get_recipes_by_category)
