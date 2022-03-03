0b89d4e1c2768f264a6cf7b59e42262814d12585
Lucas Gasque <lucasfdg@hotmail.com>
2022-03-03 17:12:03 -0300

    feature: create AuthSchema class, and refactor create user controller

--- app/schemas/auth_schema.py
+++ app/schemas/auth_schema.py
@@ -1,10 +1,22 @@
-from marshmallow import Schema, fields, post_load
-from app.models.auths_model import Auth
+from marshmallow import Schema, ValidationError, fields, post_load, pre_load, validates
+from app.models.auth_model import Auth
+
 
 class AuthSchema(Schema):
-    email = fields.Email()
-    password_hash = fields.String()
-    
+    email = fields.Email(required=True)
+    password = fields.String(required=True)
+
+    @pre_load
+    def normalize_email(self, data, **kwargs):
+        email = data.get("email")
+        data["email"] = email.lower()
+        return data
+
+    @validates("password")
+    def validate_password(self, password):
+        if len(password) < 6:
+            raise ValidationError("Error: password needs to be above 6 characthers")
+
     @post_load
     def make_auth(self, data, **kwargs):
         return Auth(**data)
