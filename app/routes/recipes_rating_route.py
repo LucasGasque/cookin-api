99056947d93ac31775d8384a5065c8b43bc9d64c
Manoela Cunha <manoela_fg@hotmail.com>
2022-03-03 12:56:35 -0300

    feat: update code recipes_rating_route

--- app/routes/recipes_rating_route.py
+++ app/routes/recipes_rating_route.py
@@ -5,8 +5,8 @@ from app.controllers.recipes_rating import register_recipes_rating, update_recip
 
 bp_recipes_rating = Blueprint("bp_recipes_rating", __name__, url_prefix="/recipes")
 
-bp_recipes_rating.post("/<int:recipe_id>/rating")(register_recipes_rating)
+bp_recipes_rating.post("/<string:recipe_id>/rating")(register_recipes_rating)
 
-bp_recipes_rating.patch("/<int:recipe_id>/rating")(update_recipes_rating)
+bp_recipes_rating.patch("/<string:recipe_id>/rating")(update_recipes_rating)
 
-bp_recipes_rating.delete("/<int:recipe_id>/rating")(delete_recipes_rating)
+bp_recipes_rating.delete("/<string:recipe_id>/rating")(delete_recipes_rating)
