45fc9985cb378489b7bd996ffb6407fe4c778d4d
Manoela Cunha <manoela_fg@hotmail.com>
2022-03-03 10:02:53 -0300

    feat: recipes_rating table creation

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -8,11 +8,10 @@ bp_api = Blueprint("api", __name__, url_prefix="/api")
 
 def init_app(app: Flask):
     # bps here
+    app.register_blueprint(bp_api)
+
     bp_api.register_blueprint(bp_auth)
     
     bp_api.register_blueprint(bp_recipes)
     
     bp_api.register_blueprint(bp_recipes_favorite)
-
-    app.register_blueprint(bp_api)
-    
