3c367075810451c533dc4730c58907b0cb16dde9
Raissa Toledo <raissalst@gmail.com>
2022-03-03 08:33:49 -0300

    feat: implementing auth services

---
+++ app/services/auth_services.py
@@ -0,0 +1,22 @@
+from app.exc.missing_valid_keys_error import MissingValidKeysError
+from app.exc.wrong_key_sent_error import WrongKeySentError
+
+def check_keys(keys):
+    valid_keys = ["name", "email", "gender", "password", "profile_photo"]
+    valid_keys_missing = []
+    for key in keys:
+        if key not in valid_keys:
+            raise WrongKeySentError(valid_keys)
+
+    for valid_key in valid_keys:
+        if valid_key not in keys and valid_key != "profile_photo":
+            valid_keys_missing.append(valid_key)
+
+    if len(valid_keys_missing) != 0:
+        raise MissingValidKeysError(valid_keys, valid_keys_missing)
+
+def check_values_types(values):
+    for value in values:
+        if type(value) != str:
+            raise AttributeError
+
