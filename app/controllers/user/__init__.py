e4176cf32bf759668977f5116b9738ee7be2514d
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-03 10:37:47 -0300

    fix: fixing routes

---
+++ app/controllers/user/__init__.py
@@ -0,0 +1,2 @@
+from .get_user_by_id import get_user_by_id
+from .get_users import get_users
--- app/routes/users_route.py
+++ app/routes/users_route.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from app.controllers import user
+from app.controllers.user import get_user_by_id, get_users
 
 bp_users = Blueprint("users", __name__, url_prefix="/users")
 
-bp_users.get("")(user.get_users)
-bp_users.get("/<string:user_id>")(user.get_user_by_id)
+bp_users.get("")(get_users)
+bp_users.get("/<string:user_id>")(get_user_by_id)
