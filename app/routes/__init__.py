8a258f893126efc4d84f69815fe551f126a106fd
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-03 14:21:59 -0300

    refactor: fix again routes from blueprint to route

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,8 +2,8 @@ from flask import Flask, Blueprint
 
 from app.routes.auth_route import bp_auth
 from app.routes.users_route import bp_users
-from app.routes.recipes_blueprint import bp_recipes
-from app.routes.recipes_favorite_blueprint import bp_recipes_favorite
+from app.routes.recipes_route import bp_recipes
+from app.routes.recipes_favorite_route import bp_recipes_favorite
 from app.routes.recipes_rating_route import bp_recipes_rating
 
 bp_api = Blueprint("api", __name__, url_prefix="/api")
