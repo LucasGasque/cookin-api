b74c0893c574dcda74c791ed56bd1da6bc53ecc6
Manoela Cunha <manoela_fg@hotmail.com>
2022-03-03 15:13:54 -0300

    feat: get_recipes controller creation

--- app/controllers/recipes/get_recipes.py
+++ app/controllers/recipes/get_recipes.py
@@ -1,4 +1,11 @@
+from http import HTTPStatus
 
+from flask import current_app, jsonify
+
+from app.models.recipes_model import Recipe
 
 def get_recipes():
-    pass
+    base_query = current_app.db.session.query(Recipe)
+    recipes = base_query.all()
+
+    return jsonify(recipes), HTTPStatus.OK
