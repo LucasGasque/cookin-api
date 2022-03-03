87f2925812c32e4c5ed13b2cf6d32d3175f85e91
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-03 09:28:28 -0300

    fix: url get route changing from str to string

--- app/routes/users_route.py
+++ app/routes/users_route.py
@@ -5,4 +5,4 @@ from app.controllers import user
 bp_users = Blueprint("users", __name__, url_prefix="/users")
 
 bp_users.get("")(user.get_users)
-bp_users.get("/<str:user_id>")(user.get_user_by_id)
+bp_users.get("/<string:user_id>")(user.get_user_by_id)
