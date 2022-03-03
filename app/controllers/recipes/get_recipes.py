5e15d34265f527eb282bda291078c67df11f0d2f
Manoela Cunha <manoela_fg@hotmail.com>
2022-03-03 17:21:06 -0300

    feat: add filters in get_recipes controller

--- app/controllers/recipes/get_recipes.py
+++ app/controllers/recipes/get_recipes.py
@@ -1,11 +1,33 @@
 from http import HTTPStatus
 
-from flask import current_app, jsonify
+from flask import jsonify, request
+
+from sqlalchemy.exc import DataError
 
 from app.models.recipes_model import Recipe
 
 def get_recipes():
-    base_query = current_app.db.session.query(Recipe)
-    recipes = base_query.all()
+    try:
+        recipes = Recipe.query.all()
 
-    return jsonify(recipes), HTTPStatus.OK
+        category = request.args.get("category", None, type=str)
+        preparation_time = request.args.get("preparation_time", None, type=str)
+        difficulty = request.args.get("difficulty", None, type=str)
+        portion_size = request.args.get("portion_size", None, type=str)
+
+        if category:
+            recipes = Recipe.query.filter_by(category=category).all()
+
+        if preparation_time:
+            recipes = Recipe.query.filter_by(preparation_time=preparation_time).all()
+
+        if difficulty:
+            recipes = Recipe.query.filter_by(difficulty=difficulty).all()
+
+        if portion_size:
+            recipes = Recipe.query.filter_by(portion_size=portion_size).all()
+
+        return jsonify(recipes), HTTPStatus.OK
+
+    except DataError as e:
+        return jsonify(dict(error=' '.join(e.orig.pgerror.split()[:8]))), HTTPStatus.BAD_REQUEST
--- app/routes/recipes_route.py
+++ app/routes/recipes_route.py
@@ -1,9 +1,7 @@
 from flask import Blueprint
 
-from app.controllers.recipes import get_recipes, get_recipes_by_category
+from app.controllers.recipes import get_recipes
 
 bp_recipes = Blueprint("bp_recipes", __name__, url_prefix="/recipes")
 
 bp_recipes.get("")(get_recipes)
-
-bp_recipes.get("/<string:category>")(get_recipes_by_category)
