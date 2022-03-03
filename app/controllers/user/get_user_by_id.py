85e97361506d8eb54b580a08f68696f12a992b42
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-03 16:46:18 -0300

    feat:get_user_by_id function done

--- app/controllers/user/get_user_by_id.py
+++ app/controllers/user/get_user_by_id.py
@@ -1,3 +1,15 @@
+from http import HTTPStatus
+from sqlite3 import DataError
 
-def get_user_by_id():
-    ...
+from flask import jsonify
+from app.models.users_model import User
+from sqlalchemy.exc import DataError
+
+def get_user_by_id(user_id: str):
+    
+    try:
+        user = User.query.filter_by(auth_id=user_id).one_or_none()
+        return jsonify(user), HTTPStatus.OK
+    except DataError: 
+        return {"error": f"user id {user_id} not found"}, HTTPStatus.NOT_FOUND
+       
