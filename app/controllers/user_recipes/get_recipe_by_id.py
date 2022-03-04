363efec65995be980d96715b8a423a8e5620a2b8
Emily <emregolao@gmail.com>
2022-03-04 18:27:08 -0300

    implements logic to get_recipe_by_id controller

--- app/controllers/user_recipes/get_recipe_by_id.py
+++ app/controllers/user_recipes/get_recipe_by_id.py
@@ -1,2 +1,12 @@
+from http import HTTPStatus
+from flask import jsonify
+from flask_jwt_extended import jwt_required
+from app.models.recipes_model import Recipe
+
+@jwt_required
 def get_recipe_by_id(recipe_id: str):
-    ...
+    try:
+        recipe = Recipe.query.filter_by(id=recipe_id).one_or_none()
+        return jsonify(recipe), HTTPStatus.OK
+    except:
+        return jsonify({"error": "recipe not found"}), HTTPStatus.NOT_FOUND
