a28f6e55b5507ac54dd2052364d421b43fb8c40b
Raissa Toledo <raissalst@gmail.com>
2022-03-04 14:42:44 -0300

    feat: changing order of conditional and adding function to check uuid

--- app/controllers/user_recipes/delete_recipe.py
+++ app/controllers/user_recipes/delete_recipe.py
@@ -2,15 +2,19 @@ from http import HTTPStatus
 
 from flask import current_app
 from flask_jwt_extended import get_jwt_identity, jwt_required
-from sqlalchemy.exc import DataError
 from werkzeug.exceptions import NotFound
 
 from app.models.recipes_model import Recipe
 from app.models.user_private_recipes_model import UserPrivateRecipe
+from app.utils.is_uuid import is_uuid
 
 
 @jwt_required()
 def delete_recipe(recipe_id: str):
+
+    if not is_uuid(recipe_id):
+        return {"msg": f"id sent is not uuid"}, HTTPStatus.BAD_REQUEST
+
     try:
         user = get_jwt_identity()
         auth_id = user["id"]
@@ -21,17 +25,14 @@ def delete_recipe(recipe_id: str):
             recipe_id=recipe_id, user_id=auth_id
         ).one_or_none()
 
-        if owner_of_searched_recipe:
-            current_app.db.session.delete(filtered_recipe)
-            current_app.db.session.commit()
-            return {}, HTTPStatus.NO_CONTENT
-
-        else:
+        if not owner_of_searched_recipe:
             return {
                 "error": "you are not allowed to delete this recipe"
-            }, HTTPStatus.NOT_FOUND
+            }, HTTPStatus.BAD_REQUEST
+
+        current_app.db.session.delete(filtered_recipe)
+        current_app.db.session.commit()
+        return {}, HTTPStatus.NO_CONTENT
 
     except NotFound:
         return {"msg": "recipe not found"}, HTTPStatus.NOT_FOUND
-    except DataError:
-        return {"msg": f"id sent is not uuid"}, HTTPStatus.BAD_REQUEST
