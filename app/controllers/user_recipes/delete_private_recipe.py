60fbc3237157810a04fbc22e666dcdd17f4a0755
Raissa Toledo <raissalst@gmail.com>
2022-03-04 14:09:28 -0300

    feat: making changes to verifications in delete recipes

--- app/controllers/user_recipes/delete_private_recipe.py
+++ app/controllers/user_recipes/delete_private_recipe.py
@@ -1,38 +1,38 @@
-from http import HTTPStatus
+# from http import HTTPStatus
 
-from flask import current_app
-from flask_jwt_extended import get_jwt_identity, jwt_required
-from sqlalchemy.exc import DataError
-from werkzeug.exceptions import NotFound
+# from flask import current_app
+# from flask_jwt_extended import get_jwt_identity, jwt_required
+# from sqlalchemy.exc import DataError
+# from werkzeug.exceptions import NotFound
 
-from app.models.auth_model import Auth
-from app.models.recipes_model import Recipe
-from app.models.user_private_recipes_model import UserPrivateRecipe
+# from app.models.auth_model import Auth
+# from app.models.recipes_model import Recipe
+# from app.models.user_private_recipes_model import UserPrivateRecipe
 
 
-@jwt_required()
-def delete_private_recipe(id: str):
-    try:
-        user = get_jwt_identity()
-        auth_id = user["id"]
+# @jwt_required()
+# def delete_private_recipe(id: str):
+#     try:
+#         user = get_jwt_identity()
+#         auth_id = user["id"]
 
-        filtered_recipe = Recipe.query.get_or_404(id)
+#         filtered_recipe = Recipe.query.get_or_404(id)
 
-        owner_of_searched_recipe = UserPrivateRecipe.query.filter_by(
-            recipe_id=id
-        ).first()
+#         owner_of_searched_recipe = UserPrivateRecipe.query.filter_by(
+#             recipe_id=id
+#         ).first()
 
-        if str(owner_of_searched_recipe.user_id) == auth_id:
-            current_app.db.session.delete(filtered_recipe)
-            current_app.db.session.commit()
-            return {}, HTTPStatus.NO_CONTENT
+#         if str(owner_of_searched_recipe.user_id) == auth_id:
+#             current_app.db.session.delete(filtered_recipe)
+#             current_app.db.session.commit()
+#             return {}, HTTPStatus.NO_CONTENT
 
-        else:
-            return {
-                "error": "you are not allowed to delete this recipe"
-            }, HTTPStatus.BAD_REQUEST
+#         else:
+#             return {
+#                 "error": "you are not allowed to delete this recipe"
+#             }, HTTPStatus.BAD_REQUEST
 
-    except NotFound:
-        return {"msg": "recipe not found"}, HTTPStatus.NOT_FOUND
-    except DataError:
-        return {"msg": f"id sent is not uuid"}
+#     except NotFound:
+#         return {"msg": "recipe not found"}, HTTPStatus.NOT_FOUND
+#     except DataError:
+#         return {"msg": f"id sent is not uuid"}
--- app/controllers/user_recipes/delete_recipe.py
+++ app/controllers/user_recipes/delete_recipe.py
@@ -1,2 +1,36 @@
+from http import HTTPStatus
+
+from flask import current_app
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from sqlalchemy.exc import DataError
+from werkzeug.exceptions import NotFound
+
+from app.models.recipes_model import Recipe
+from app.models.user_private_recipes_model import UserPrivateRecipe
+
+@jwt_required()
 def delete_recipe(recipe_id: str):
-    ...
+    try:
+        user = get_jwt_identity()
+        auth_id = user["id"]
+
+        filtered_recipe = Recipe.query.get_or_404(recipe_id)
+
+        owner_of_searched_recipe = UserPrivateRecipe.query.filter_by(
+            recipe_id=recipe_id, auth_id=auth_id
+        ).one_or_none()
+
+        if owner_of_searched_recipe:
+            current_app.db.session.delete(filtered_recipe)
+            current_app.db.session.commit()
+            return {}, HTTPStatus.NO_CONTENT
+
+        else:
+            return {
+                "error": "you are not allowed to delete this recipe"
+            }, HTTPStatus.NOT_FOUND
+
+    except NotFound:
+        return {"msg": "recipe not found"}, HTTPStatus.NOT_FOUND
+    except DataError:
+        return {"msg": f"id sent is not uuid"}
