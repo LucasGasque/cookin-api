f71b7d36992e9fdf7b13b4e0deeb01b2489a4038
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-04 14:52:02 -0300

    refactor: update_recipe import commented

--- app/controllers/user_recipes/__init__.py
+++ app/controllers/user_recipes/__init__.py
@@ -4,4 +4,4 @@ from .delete_recipe import delete_recipe
 from .get_recipe_by_id import get_recipe_by_id
 from .get_recipes import get_recipes
 from .share_recipe import share_recipe
-from .update_recipe import update_recipe
+# from .update_recipe import update_recipe
