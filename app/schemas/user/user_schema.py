4e93ce9aad68db253c0cfd2a15ea8234b93ab164
Lucas Gasque <lucasfdg@hotmail.com>
2022-03-07 09:24:45 -0300

    fix

---
+++ app/schemas/user/user_schema.py
@@ -0,0 +1,27 @@
+from marshmallow import Schema, ValidationError, fields, post_load, pre_load, validates
+from app.models.users_model import User
+
+
+class UserSchema(Schema):
+    auth_id = fields.UUID(required=True)
+    name = fields.String(required=True)
+    gender = fields.String(required=True)
+    profile_photo = fields.Url(allow_none=True)
+
+    @pre_load
+    def receive_data(self, data, **kwargs):
+        gender = data.get("gender")
+        if type(gender) == str:
+            data["gender"] = gender.title()
+        return data
+
+    @validates("gender")
+    def validate_gender(self, gender):
+        if gender not in ["Feminino", "Masculino", "Outro"]:
+            raise ValidationError("Error: gender must be either 'Feminino', 'Masculino' or 'Outro'")
+
+    @post_load
+    def make_user(self, data, **kwargs):
+        name = data.get("name")
+        data["name"] = name.title()
+        return User(**data)
