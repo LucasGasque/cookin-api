8a70966020f8e18ce2f9938bae585f40dcf8c61c
Raissa Toledo <raissalst@gmail.com>
2022-03-07 17:44:48 -0300

    feat: adding exception

--- app/controllers/favorite_recipes/get_favorite_recipes.py
+++ app/controllers/favorite_recipes/get_favorite_recipes.py
@@ -8,25 +8,30 @@ from app.configs.database import db
 from app.models.favorite_recipes_model import FavoriteRecipe
 from app.models.recipes_model import Recipe
 from app.schemas.favorite_recipes import GetFavoriteRecipesSchema
+from marshmallow import ValidationError
 
 
 @jwt_required()
 def get_favorite_recipes():
-    session: Session = db.session
-    user = get_jwt_identity()
-    auth_id = user["id"]
+    try:
+        session: Session = db.session
+        user = get_jwt_identity()
+        auth_id = user["id"]
 
-    GetFavoriteRecipesSchema().load({"auth_id": auth_id})
+        GetFavoriteRecipesSchema().load({"auth_id": auth_id})
 
-    array_of_user_favorites = []
+        array_of_user_favorites = []
 
-    recipes_list = session.query(FavoriteRecipe).filter_by(user_id=auth_id).all()
+        recipes_list = session.query(FavoriteRecipe).filter_by(user_id=auth_id).all()
 
-    for item in recipes_list:
-        recipe_found_in_recipes = (
-            session.query(Recipe).filter_by(id=item.recipe_id).first()
-        )
-        if recipe_found_in_recipes.public == True:
-            array_of_user_favorites.append(recipe_found_in_recipes)
+        for item in recipes_list:
+            recipe_found_in_recipes = (
+                session.query(Recipe).filter_by(id=item.recipe_id).first()
+            )
+            if recipe_found_in_recipes.public == True:
+                array_of_user_favorites.append(recipe_found_in_recipes)
 
-    return jsonify(array_of_user_favorites), HTTPStatus.OK
+        return jsonify(array_of_user_favorites), HTTPStatus.OK
+    
+    except ValidationError as error:
+        return {"Error": error.args}, HTTPStatus.BAD_REQUEST
