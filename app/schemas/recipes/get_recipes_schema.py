9ce2afc7278a797ba6ec30d6a97598f1916c0642
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-07 11:18:36 -0300

    refactor: changed prep_time and portion_size type

--- app/schemas/recipes/get_recipes_schema.py
+++ app/schemas/recipes/get_recipes_schema.py
@@ -8,6 +8,6 @@ class GetRecepisSchema(Schema):
     page = fields.Integer(required=True)
     per_page = fields.Integer(required=True)
     category = EnumField(CategoryEnumType, allow_none=True)
-    preparation_time = fields.String(allow_none=True)
+    preparation_time = fields.Integer(allow_none=True)
     difficulty = EnumField(DifficultyEnumType, allow_none=True)
-    portion_size = fields.String(allow_none=True)
+    portion_size = fields.Integer(allow_none=True)
