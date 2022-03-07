2843498c29dce1824824f9f73618ab24ff18d998
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-07 11:20:45 -0300

    fix: changing schema validation

--- app/routes/recipes_rating_route.py
+++ app/routes/recipes_rating_route.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 
-from app.controllers.recipes_rating import register_recipes_rating, update_recipes_rating, delete_recipes_rating
+from app.controllers.recipes_rating import create_recipes_rating, update_recipes_rating, delete_recipes_rating
 
 bp_recipes_rating = Blueprint("bp_recipes_rating", __name__, url_prefix="/recipes")
 
-bp_recipes_rating.post("/<string:recipe_id>/rating")(register_recipes_rating)
+bp_recipes_rating.post("/<string:recipe_id>/rating")(create_recipes_rating)
 
 bp_recipes_rating.patch("/<string:recipe_id>/rating")(update_recipes_rating)
 
