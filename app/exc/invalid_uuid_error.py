c0ec017e15e4d10b09b1015ce3e7424a9f6c45c1
Lucas Gasque <lucasfdg@hotmail.com>
2022-03-07 10:54:28 -0300

    fix

--- app/exc/invalid_uuid_error.py
+++
@@ -1,7 +0,0 @@
-class InvalidUuidError(Exception):
-     def __init__(self, array_of_keys: list):
-        self.message = {
-            "Error": "Invalid uuid"
-        }
-
-        super().__init__(self.message)
