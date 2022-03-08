369ae860de5c3683b8a8947cd6103405dddd8a9d
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-08 11:43:51 -0300

    refactor: changed category and difficulty fields

--- app/schemas/user_recipes/update_private_recipe_schema.py
+++ app/schemas/user_recipes/update_private_recipe_schema.py
@@ -1,19 +1,20 @@
 from uuid import UUID
-from marshmallow import Schema, fields
-from marshmallow_enum import EnumField
-
-from app.utils.category_enum_type import CategoryEnumType
-from app.utils.difficulty_enum_type import DifficultyEnumType
+from marshmallow import Schema, fields, validate
 
 
 class UpdatePrivateRecipeSchema(Schema):
     
     title = fields.String()
     ingredients = fields.List(cls_or_instance=fields.String)
-    instructions = fields.List(cls_or_instance=fields.String)    
-    category = EnumField(CategoryEnumType, allow_none=True)
+    instructions = fields.List(cls_or_instance=fields.String)   
+    category = fields.String(validate=validate.OneOf([
+        "Doce", "Salgado", "Bebida"
+    ]))
     preparation_time = fields.Integer()
-    difficulty = EnumField(DifficultyEnumType, allow_none=True)    
+    difficulty = fields.String(validate=validate.OneOf([
+        "Fácil", "Intermediário", "Difícil"
+    ]))    
     portion_size = fields.Integer()
     image_url = fields.Url()
     recipe_id = fields.UUID()
+    
