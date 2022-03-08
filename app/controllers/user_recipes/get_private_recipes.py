2db9e68c4323c1e89328bb785e7625f021ce47ba
Emily <emregolao@gmail.com>
2022-03-08 10:27:08 -0300

    implements logic to get_private_recipes controller

--- app/controllers/user_recipes/get_private_recipes.py
+++ app/controllers/user_recipes/get_private_recipes.py
@@ -1,2 +1,15 @@
+from http import HTTPStatus
+from flask import jsonify
+from flask_jwt_extended import get_jwt_identity, jwt_required
+from app.models.user_private_recipes_model import UserPrivateRecipe
+
 def get_private_recipes():
-    ...
+    
+    user = get_jwt_identity()
+    recipes = UserPrivateRecipe.query.filter_by(user_id = user["id"]).all()
+
+    if not user["id"]:
+        return jsonify({"error": "user not found"}), HTTPStatus.NOT_FOUND
+
+    return jsonify(recipes), HTTPStatus.OK
+
