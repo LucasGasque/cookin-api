08478af643f0c4200ca61d640e47c75fdf43ae9c
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-08 09:18:16 -0300

    fix: deleting working now

--- app/controllers/recipes_rating/delete_recipes_rating.py
+++ app/controllers/recipes_rating/delete_recipes_rating.py
@@ -1,36 +1,22 @@
-
 from http import HTTPStatus
 
-from flask import current_app
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from marshmallow import ValidationError
-from sqlalchemy.exc import DataError
+from sqlalchemy.orm import Session
+from app.configs.database import db
 
-from app.models.recipes_model import Recipe
+from sqlalchemy.orm.exc import NoResultFound
+
 from app.models.recipes_rating_model import RecipesRating
-from app.models.user_private_recipes_model import UserPrivateRecipe
-from app.schemas.recipes_rating.delete_recipes_rating_schema import \
-    RatingRecipeDeleteSchema
+from app.schemas.recipes_rating.delete_recipes_rating_schema import RatingRecipeDeleteSchema
 
 
-@jwt_required
+@jwt_required()
 def delete_recipes_rating(recipe_id: str):
 
     try:
-
         user_authorized = get_jwt_identity()
         auth_id = user_authorized["id"]
-
-        Recipe.query.get_or_404(recipe_id)
-
-        owner_of_searched_recipe = UserPrivateRecipe.query.filter_by(
-                recipe_id=recipe_id, user_id=auth_id
-            ).one_or_none()
-
-        if owner_of_searched_recipe:
-                return {
-                    "Error": "you are not allowed to delete this recipe rate"
-                }, HTTPStatus.BAD_REQUEST
 
         RatingRecipeDeleteSchema().load({"recipe_id": recipe_id, "auth_id": auth_id})
 
@@ -38,13 +24,18 @@ def delete_recipes_rating(recipe_id: str):
                 recipe_id=recipe_id, user_id=auth_id
             ).one_or_none()
 
-        current_app.db.session.delete(recipe_rate_to_be_deleted)
-        current_app.db.session.commit()
+        if not recipe_rate_to_be_deleted:
+            raise NoResultFound
+        
+        session: Session = db.session
+
+        session.delete(recipe_rate_to_be_deleted)
+        session.commit()
 
         return "", HTTPStatus.NO_CONTENT
 
     except ValidationError as error:
         return {"Error": error.args}, HTTPStatus.BAD_REQUEST
 
-    except DataError:
-        return {"Error": "recipe not found"}, HTTPStatus.NOT_FOUND
+    except NoResultFound:
+        return {"Error": "User cannot delete this rating"}, HTTPStatus.NOT_FOUND
--- app/schemas/recipes_rating/delete_recipes_rating_schema.py
+++ app/schemas/recipes_rating/delete_recipes_rating_schema.py
@@ -1,11 +1,5 @@
-from marshmallow import Schema, fields, post_load
-
-from app.models.recipes_rating_model import RecipesRating
+from marshmallow import Schema, fields
 
 class RatingRecipeDeleteSchema(Schema):
     recipe_id = fields.UUID(required=True)
     auth_id = fields.UUID(required=True)
-
-@post_load
-def delete_rate(self, data, **kwargs):
-    return RecipesRating(data)
