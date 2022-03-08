fb7e758bd7607dd0f97ee1076bcf2635c0699599
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-08 14:02:53 -0300

    fix:try and excepts added

--- app/controllers/user/get_users.py
+++ app/controllers/user/get_users.py
@@ -1,4 +1,6 @@
 from http import HTTPStatus
+from marshmallow import ValidationError
+from sqlalchemy.orm.exc import NoResultFound
 
 from flask import jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -11,16 +13,24 @@ from app.schemas.user.get_users_schema import UserGetSchema
 
 @jwt_required()
 def get_users():
-        
-    authorized_user = get_jwt_identity()
-    auth_id = authorized_user["id"]
 
-    UserGetSchema().load({"auth_id": auth_id})
+    try:
+            
+        authorized_user = get_jwt_identity()
+        auth_id = authorized_user["id"]
 
-    session: Session = db.session
-    user = session.query(User).all()
+        UserGetSchema().load({"auth_id": auth_id})
 
-    if not user:
+        session: Session = db.session
+        user = session.query(User).all()
+
+        if not user:
+            raise NoResultFound
+
+        return jsonify({"Users": user}), HTTPStatus.OK
+
+    except ValidationError as error:
+        return {"Error":error.args}, HTTPStatus.BAD_REQUEST
+
+    except NoResultFound:
         return {"Error": "No users found"}, HTTPStatus.NOT_FOUND
-
-    return jsonify({"Users": user}), HTTPStatus.OK
