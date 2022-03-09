8d1a3619c4fbed90d6e81780e7027f77290ec255
Raissa Toledo <raissalst@gmail.com>
2022-03-09 12:34:53 -0300

    fix: using black

--- app/controllers/favorite_recipes/get_favorite_recipes.py
+++ app/controllers/favorite_recipes/get_favorite_recipes.py
@@ -23,23 +23,32 @@ def get_favorite_recipes():
         session: Session = db.session
         recipes_list = session.query(FavoriteRecipe).filter_by(user_id=auth_id).all()
 
-        #lista de todas as ids favoritadas pelo usuário:
+        # lista de todas as ids favoritadas pelo usuário:
         recipes_ids_list = [item.recipe_id for item in recipes_list]
 
         user_id_and_recipe_list = (
-            session.query(UserPrivateRecipe).filter(UserPrivateRecipe.recipe_id.in_(recipes_ids_list)).all()
+            session.query(UserPrivateRecipe)
+            .filter(UserPrivateRecipe.recipe_id.in_(recipes_ids_list))
+            .all()
         )
-        print(len(user_id_and_recipe_list))
 
-        #lista com ids favoritadas e de autoria do usuário:
-        recipes_ids_owned_by_logged_user = [item.recipe_id for item in user_id_and_recipe_list if str(item.user_id) == auth_id]
+        # lista com ids favoritadas e de autoria do usuário:
+        recipes_ids_owned_by_logged_user = [
+            item.recipe_id
+            for item in user_id_and_recipe_list
+            if str(item.user_id) == auth_id
+        ]
 
-        #lista das receitas favoritas do usuário
+        # lista das receitas favoritas do usuário
         favorite_recipes_list = (
             session.query(Recipe).filter(Recipe.id.in_(recipes_ids_list)).all()
         )
 
-        favorite_recipes_and_public_list = [item for item in favorite_recipes_list if item.public == True or item.id in recipes_ids_owned_by_logged_user]
+        favorite_recipes_and_public_list = [
+            item
+            for item in favorite_recipes_list
+            if item.public == True or item.id in recipes_ids_owned_by_logged_user
+        ]
 
         return jsonify(favorite_recipes_and_public_list), HTTPStatus.OK
 
