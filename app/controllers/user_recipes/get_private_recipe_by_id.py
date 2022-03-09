4f63277db9ae988a3ecc195595c8481492b41d9f
Roberto Bressanelli <rbressanelli@gmail.com>
2022-03-09 16:15:38 -0300

    refactor: code format

--- app/controllers/user_recipes/get_private_recipe_by_id.py
+++ app/controllers/user_recipes/get_private_recipe_by_id.py
@@ -7,16 +7,18 @@ from sqlalchemy.orm.exc import NoResultFound
 from marshmallow.exceptions import ValidationError
 
 from app.models.recipes_model import Recipe
-from app.schemas.user_recipes.get_private_recipe_by_id_schema import GetPrivateRecipeByIdSchema
+from app.schemas.user_recipes.get_private_recipe_by_id_schema import (
+    GetPrivateRecipeByIdSchema,
+)
 from app.models.user_private_recipes_model import UserPrivateRecipe
 
 
 @jwt_required()
-def get_private_recipe_by_id(recipe_id: str):    
+def get_private_recipe_by_id(recipe_id: str):
     try:
         user = get_jwt_identity()
         auth_id = user["id"]
-        GetPrivateRecipeByIdSchema().load({"auth_id": auth_id, "recipe_id": recipe_id })
+        GetPrivateRecipeByIdSchema().load({"auth_id": auth_id, "recipe_id": recipe_id})
 
         private_recipe: UserPrivateRecipe = UserPrivateRecipe.query.filter_by(
             recipe_id=recipe_id, user_id=auth_id
@@ -26,14 +28,10 @@ def get_private_recipe_by_id(recipe_id: str):
 
         recipe = Recipe.query.filter_by(id=recipe_id).first()
 
-
         return jsonify(recipe), HTTPStatus.OK
 
     except NoResultFound:
         return jsonify({"Error": "recipe not found"}), HTTPStatus.NOT_FOUND
-    
+
     except ValidationError as error:
-        return jsonify({
-            'Error': error.args
-        }), HTTPStatus.BAD_REQUEST
-    
+        return jsonify({"Error": error.args}), HTTPStatus.BAD_REQUEST
