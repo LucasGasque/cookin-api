811909f7efa705a6bc11a447b87b45843605d596
larissakoliveira <oliveir5uwm@gmail.com>
2022-03-10 16:16:06 -0300

    fix: removing secondary attribute from rating relationship

--- app/controllers/recipes_rating/create_recipe_rating.py
+++ app/controllers/recipes_rating/create_recipe_rating.py
@@ -28,8 +28,6 @@ def create_recipe_rating(recipe_id: str):
 
         recipe_rated = RecipeRateSchema().load(data)
 
-        filtered_recipe = Recipe.query.get_or_404(recipe_id)
-
         owner_of_searched_recipe = UserPrivateRecipe.query.filter_by(
             recipe_id=recipe_id, user_id=auth_id
         ).one_or_none()
